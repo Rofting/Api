d8652cf6dd97495966e05d7d6c8ea7b1ec3b627c
Jose <a27941@svalero.com>
2024-03-11 23:48:06 +0100

    Añadimos menu a la clase principal

---
+++ src/main/java/org/example/CRUD.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Delete {
+    public static void deleteQuery(Connection connection, String query){
+        try (Statement statement = connection.createStatement()) {
+            int rowsAffected = statement.executeUpdate(query);
+            System.out.println("Filas afectadas: " + rowsAffected);
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta de actualización: " + e.getMessage());
+        }
+    }
+}
+public class Update {
+
+    public static void updateQuery(Connection connection, String query) {
+        try (Statement statement = connection.createStatement()) {
+            int rowsAffected = statement.executeUpdate(query);
+            System.out.println("Filas afectadas: " + rowsAffected);
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la consulta de actualización: " + e.getMessage());
+        }
+    }
+}
